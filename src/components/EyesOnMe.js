0c79154dbfd3eb72e31ea16fa95b783f425f1c62
Will-Gon <will.colocho@gmail.com>
2020-12-14 12:00:43 -0800

    commit tree d34474d451bb04c35b723c60cdc6e43aa8bef6f7

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+
+    goodJob = () => {
+        console.log('Good!')
+    }
+
+    heyLook = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    
+    render() {
+        return (
+            <button
+            onFocus={this.goodJob}
+            onBlur={this.heyLook}>
+            </button>
+        )
+    }
+}
+
